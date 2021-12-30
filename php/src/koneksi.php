b96e20b5e285cd27ec4b62d69f9fe21353892d77
= <=>
2021-12-30 18:23:17 +0700

    first commit

--- php/src/koneksi.php
+++ php/src/koneksi.php
@@ -1,5 +1,12 @@
 <?php
-  
-  $db = new mysqli("db", "MYSQL_USER", "MYSQL_PASSWORD", "MYSQL_DATABASE");
-
+define('DB_SERVER','10.0.0.48');
+define('DB_USER','admin');
+define('DB_PASS' ,'Sumbermanjng20_');
+define('DB_NAME','mahasiswa');
+$conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>
